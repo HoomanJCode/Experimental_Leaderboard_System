f5fc00387c31b9f52979a77582cfebd5cab62874
Hooman Jalalpoor <hooman.jalalpoor@gmail.com>
2025-04-20 09:32:12 +0330

    Some fixes like leaderboard repo

--- LeaderboardSystem/Assets/Scripts/Repositories/AvatarRepository.cs
+++ LeaderboardSystem/Assets/Scripts/Repositories/AvatarRepository.cs
@@ -2,45 +2,47 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Repositories.Models;
+using UnityEngine;
 
 namespace Repositories
 {
     public class AvatarRepository : IAvatarRepository
     {
-        private readonly IStorageAdapter<byte[]> _storage;
-        private readonly string mainPath;
+        private readonly IStorageAdapter<byte[]> _storage = new PhotoFileAdapter();
+        private string MainPath => "C:\\...";
 
-        public AvatarRepository(IStorageAdapter<byte[]> storage, string folderPath)
+        public AvatarRepository()
         {
-            _storage = storage ?? throw new ArgumentNullException(nameof(storage));
-            mainPath=folderPath;
         }
 
         public async Task AddAsync(PlayerAvatar avatar)
         {
-            if (_storage.Exists(avatar.PlayerId.ToString()))
+            var path = Path.Combine(MainPath, avatar.PlayerId.ToString());
+            if (_storage.Exists(path))
                 throw new InvalidOperationException($"Avatar for player {avatar.PlayerId} already exists");
 
-            await _storage.SaveAsync(Path.Combine(mainPath, avatar.PlayerId.ToString()), avatar.ProfileImage);
+            await _storage.SaveAsync(Path.Combine(MainPath, path), avatar.ProfileImage);
         }
 
         public async Task UpdateAsync(PlayerAvatar avatar)
         {
-            if (!_storage.Exists(avatar.PlayerId.ToString()))
+            var path = Path.Combine(MainPath, avatar.PlayerId.ToString());
+            if (!_storage.Exists(path))
                 throw new InvalidOperationException($"Avatar for player {avatar.PlayerId} not found");
 
-            await _storage.SaveAsync(Path.Combine(mainPath, avatar.PlayerId.ToString()), avatar.ProfileImage);
+            await _storage.SaveAsync(Path.Combine(MainPath, path), avatar.ProfileImage);
         }
 
         public async Task<PlayerAvatar> GetByIdAsync(int playerId)
         {
-            var imageData = await _storage.LoadAsync(Path.Combine(mainPath, playerId.ToString()));
+            var imageData = await _storage.LoadAsync(Path.Combine(MainPath, playerId.ToString()));
             return new PlayerAvatar(playerId, imageData);
         }
 
         public async Task DeleteAsync(int playerId)
         {
-            _storage.Delete(playerId.ToString());
+            var path = Path.Combine(MainPath, playerId.ToString());
+            if (_storage.Exists(path)) _storage.Delete(path);
         }
     }
 }
