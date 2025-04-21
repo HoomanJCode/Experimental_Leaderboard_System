4ed2de48c6e187910b3549e764d4e5a164794f5c
Hooman Jalalpoor <hooman.jalalpoor@gmail.com>
2025-04-21 04:49:02 +0330

    photofileAdapter tests created

---
+++ LeaderboardSystem/Assets/Scripts/Storage_Tests/PhotoFileAdapterTests.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PhotoFileAdapterTests
+{
+    [Test]
+    [Timeout(2000)]
+    public void Exists_ReturnsFalseForNonExistentFile()
+    {
+        var adapter = new PhotoFileAdapter();
+        Assert.IsFalse(adapter.Exists("nonexistent_file.dat"));
+    }
+
+    [UnityTest]
+    [Timeout(2000)]
+    public IEnumerator SaveAsync_CreatesFile()
+    {
+        var adapter = new PhotoFileAdapter();
+        var testFilePath = Path.Combine(Application.persistentDataPath, nameof(PhotoFileAdapterTests) + "." + nameof(SaveAsync_CreatesFile)+ ".dat");
+        var testImageData = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05 };
+        yield return adapter.SaveAsync(testFilePath, testImageData);
+        Assert.IsTrue(File.Exists(testFilePath));
+        File.Delete(testFilePath);
+    }
+
+    [UnityTest]
+    [Timeout(2000)]
+    public IEnumerator LoadAsync_ReturnsSavedData()
+    {
+        var adapter = new PhotoFileAdapter();
+        var testFilePath = Path.Combine(Application.persistentDataPath, nameof(PhotoFileAdapterTests) + "." + nameof(LoadAsync_ReturnsSavedData)+ ".dat");
+        var testImageData = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05 };
+        yield return adapter.SaveAsync(testFilePath, testImageData);
+        var loadTask = adapter.LoadAsync(testFilePath);
+        yield return new WaitUntil(() => loadTask.IsCompleted);
+        //yield return loadTask;
+
+        Assert.AreEqual(testImageData, loadTask.Result);
+        File.Delete(testFilePath);
+    }
+
+    [Test]
+    [Timeout(2000)]
+    public void Delete_RemovesFile()
+    {
+        var adapter = new PhotoFileAdapter();
+        var testFilePath = Path.Combine(Application.persistentDataPath, nameof(PhotoFileAdapterTests) + "." + nameof(Delete_RemovesFile)+ ".dat");
+        var testImageData = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05 };
+        File.WriteAllBytes(testFilePath, testImageData);
+        adapter.Delete(testFilePath);
+        Assert.IsFalse(File.Exists(testFilePath));
+        File.Delete(testFilePath);
+    }
+    [UnityTest]
+    [Timeout(2000)]
+    public IEnumerator LoadAsync_ThrowsForNonExistentFile()
+    {
+        var adapter = new PhotoFileAdapter();
+        var loadTask = adapter.LoadAsync("nonexistent_file.dat");
+        yield return new WaitUntil(() => loadTask.IsCompleted);
+
+        Assert.IsTrue(loadTask.IsFaulted);
+        Assert.IsNotNull(loadTask.Exception);
+        Assert.IsInstanceOf<FileNotFoundException>(loadTask.Exception.InnerException);
+    }
+}
