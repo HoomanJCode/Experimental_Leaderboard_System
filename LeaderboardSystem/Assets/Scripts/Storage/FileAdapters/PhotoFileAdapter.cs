e83ef98fe8bf01f949c1bffa15edd6e3689d1d88
Hooman Jalalpoor <hooman.jalalpoor@gmail.com>
2025-04-23 00:24:25 +0330

    made filesystem async safe-thread

---
+++ LeaderboardSystem/Assets/Scripts/Storage/FileAdapters/PhotoFileAdapter.cs
@@ -0,0 +1,16 @@
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+public class PhotoFileAdapter : FileSystemAdapter<byte[]>
+{
+    protected override Task<byte[]> ReadFromFileAsync(string filePath)
+    {
+        return File.ReadAllBytesAsync(filePath);
+    }
+
+    protected override Task WriteToFileAsync(string filePath, byte[] data)
+    {
+        return File.WriteAllBytesAsync(filePath, data);
+    }
+}
